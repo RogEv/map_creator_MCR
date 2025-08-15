dbfb5925d9b118875fa1dcbd6e4fa127503061ff
eRozhkov <rogev1989@gmail.com>
2025-08-15 22:35:44 +0300

    Initial Commit

---
+++ map_creator_MCR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  map_creator_MCR
+//
+//  Created by Evgeni Rozkov on 15.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map_creator_MCR/map_creator_MCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_creator_MCRApp.swift
+//  map_creator_MCR
+//
+//  Created by Evgeni Rozkov on 15.08.25.
+//
+
+import SwiftUI
+
+@main
+struct map_creator_MCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
